250c8c71bb270113467a8fecb1c1330bb8cd2227
javaExactus <romulo.neves@exactus.com.br>
2025-05-05 21:00:58 -0300

    first commit

---
+++ src/main/java/br/com/romulo/ms_estudo_spring_batch/MsEstudoSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package br.com.romulo.ms_estudo_spring_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEstudoSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEstudoSpringBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/romulo/ms_estudo_spring_batch/MsEstudoSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.romulo.ms_estudo_spring_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEstudoSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
